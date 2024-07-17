18e69b1de2ec489fe59c54928c789706a1ac6ae1
Ajayendra2705 <ajayendrabansod2705@gmail.com>
2024-07-17 10:37:34 +0530

    Delete Portfolio_website directory

--- Portfolio_website/Preloader/script.js
+++
@@ -1,8 +0,0 @@
-// Get the preloader element
-const preloader = document.getElementById('preloader');
-
-// Hide the preloader after 2 seconds
-setTimeout(() => {
-  preloader.style.display = 'none';
-  window.location.href = 'http://127.0.0.1:5500/Home_page/index.html'
-}, 2000); // 2000 milliseconds = 2 seconds
